f736a8cfaa556d8eb0884e10f3279604cf9e2d5d
moffd234 <moffd234@gmail.com>
2024-03-30 11:26:57 -0400

    Worked on implentation of methods

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,33 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<T>{
+    T[] array;
+
+    public ArrayUtility(T[] inputArray) {
+        array = inputArray;
+    }
+
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+        Object[] newArr = new Object[arrayToMerge.length + array.length];
+        System.arraycopy(arrayToMerge,0,newArr,0,arrayToMerge.length);
+        System.arraycopy(array, 0, newArr, arrayToMerge.length, array.length);
+        for(T t: arrayToMerge){
+            System.out.println(t);
+        }
+        return null;
+    }
+
+    public T removeValue(T valueToRemove) {
+        return (T) array;
+    }
+
+    public T getNumberOfOccurrences(T valueToEvaluate) {
+        return null;
+    }
 }
