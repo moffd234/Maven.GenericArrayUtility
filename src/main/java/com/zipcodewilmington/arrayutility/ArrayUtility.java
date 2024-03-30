a55ada550734ed58a04d899ec1743221a1dec059
moffd234 <moffd234@gmail.com>
2024-03-30 17:27:24 -0400

    Setup all the methods so the tests compile. Also completed countDupMerge and GetNumOcc

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -15,19 +15,38 @@ public class ArrayUtility<T>{
 
     public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
         Object[] newArr = new Object[arrayToMerge.length + array.length];
+
+        // Combine the two arrays into the newArr
         System.arraycopy(arrayToMerge,0,newArr,0,arrayToMerge.length);
         System.arraycopy(array, 0, newArr, arrayToMerge.length, array.length);
-        for(T t: arrayToMerge){
-            System.out.println(t);
+
+        // Find number of times valueToEvaluate occurs
+        int total = 0;
+        for(int i = 0; i < newArr.length; i++){
+            if(newArr[i] == valueToEvaluate){
+                total ++;
+            }
         }
-        return null;
+        return total;
     }
 
-    public T removeValue(T valueToRemove) {
-        return (T) array;
+    public T[] removeValue(T valueToRemove) {
+        return array;
     }
 
-    public T getNumberOfOccurrences(T valueToEvaluate) {
-        return null;
+    public Integer getNumberOfOccurrences(T valueToEvaluate) {
+        int total = 0;
+        for(T t : array){
+            if(t == valueToEvaluate){
+                total += 1;
+            }
+        }
+        return total;
     }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge) {
+        return array[1];
+    }
+
+
 }
